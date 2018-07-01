63fb19d773cbc5e24bfec91461362c0749bb032c
Jake Goulding <jake.goulding@integer32.com>
2018-07-01 12:18:07 -0400

    Track gist state

--- ui/frontend/reducers/output/sharedStateManagement.ts
+++ ui/frontend/reducers/output/sharedStateManagement.ts
@@ -1,9 +1,21 @@
-export function start(zeroState, state) {
-  const { requestsInProgress } = state;
-  return { ...zeroState, requestsInProgress: requestsInProgress + 1 };
+export interface RequestsInProgress {
+  requestsInProgress: number;
 }
 
-export function finish(state, newState = {}) {
+type WithoutRequests<S> = Pick<S, Exclude<keyof S, keyof RequestsInProgress>>;
+
+export function start<S extends RequestsInProgress>(
+  zeroState: S,
+  state: S,
+): S {
   const { requestsInProgress } = state;
-  return { ...state, ...newState, requestsInProgress: requestsInProgress - 1 };
+  return Object.assign(zeroState, { requestsInProgress: requestsInProgress + 1 });
+}
+
+export function finish<S extends RequestsInProgress>(
+  state: S,
+  newState?: WithoutRequests<S>,
+): S {
+  const { requestsInProgress } = state;
+  return Object.assign(state, newState, { requestsInProgress: requestsInProgress - 1 });
 }
