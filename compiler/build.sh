d691793a0f5520650a71357cd4a8682cb96c23e5
CjS77 <CjS77@users.noreply.github.com>
2020-03-31 13:03:53 +0200

    Update build process for Tari crates
    * Include required ubunut packages and libraries
    * Update Docker files

--- compiler/build.sh
+++ compiler/build.sh
@@ -3,31 +3,32 @@
 set -euv -o pipefail
 
 # For Tari stuff, only nightly will work
-channels_to_build="${CHANNELS_TO_BUILD-nightly}"
+channels_to_build=`cat base/rust-toolchain`
 tools_to_build="${TOOLS_TO_BUILD-rustfmt clippy}"
 perform_push="${PERFORM_PUSH-false}"
 
 repository="quay.io/tarilabs"
 
-for channel in $channels_to_build; do
-    cd "base"
-
-    image_name="rust-${channel}"
-    full_name="${repository}/${image_name}"
-
-#    docker pull "${full_name}"
-    docker build -t "${full_name}" \
-           --cache-from "${full_name}" \
-           --build-arg channel="${channel}" \
-           .
-    docker tag "${full_name}" "${image_name}"
-
-    if [[ "${perform_push}" == 'true' ]]; then
-        docker push "${full_name}"
-    fi
-
-    cd ..
-done
+echo "Building ${channels_to_build}"
+#for channel in $channels_to_build; do
+#    cd "base"
+#
+#    image_name="rust-${channel}"
+#    full_name="${repository}/${image_name}"
+#
+##    docker pull "${full_name}"
+#    docker build -t "${full_name}" \
+#           --cache-from "${full_name}" \
+#           --build-arg channel="${channel}" \
+#           .
+#    docker tag "${full_name}" "${image_name}"
+#
+#    if [[ "${perform_push}" == 'true' ]]; then
+#        docker push "${full_name}"
+#    fi
+#
+#    cd ..
+#done
 
 crate_api_base=https://crates.io/api/v1/crates
 
